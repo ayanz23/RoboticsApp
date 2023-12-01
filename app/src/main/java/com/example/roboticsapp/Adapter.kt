770e66fb6c8364add656bd3bb13102dfcd305aa4
ayanz23 <ayanzia10@gmail.com>
2023-12-01 09:20:56 -0500

    commit #3

--- app/src/main/java/com/example/roboticsapp/Adapter.kt
+++ app/src/main/java/com/example/roboticsapp/Adapter.kt
@@ -5,24 +5,67 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.roboticsapp.databinding.ViewBinding
 
-class Adapter: RecyclerView.Adapter<Adapter.myViewHolder>() {
+class Adapter(private val binding: ViewBinding, val robot:List<RobotData>): RecyclerView.Adapter<Adapter.myViewHolder>() {
 
-    class myViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+    class myViewHolder(binding: ViewBinding, view: View) : RecyclerView.ViewHolder(view) {
+        var title = binding.title
+        var robotimg = binding.logo
+        var galaxy = binding.galaxy
+        var distance = binding.distance
+        var gravity = binding.gravity
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myViewHolder {
         var itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.view, parent, false)
-        return myViewHolder(itemView)
+        return myViewHolder(binding, itemView)
     }
 
     override fun onBindViewHolder(holder: myViewHolder, position: Int) {
+        var dummyImage: Int? = null
+        holder.title.text = robot[position].title
+        holder.galaxy.text = robot[position].galaxy
+        holder.distance.text = robot[position].distance + " m km"
+        holder.gravity.text = robot[position].title + " m/ss"
 
+        when (robot[position].title!!.toLowerCase()) {
+            "mars" -> {
+                dummyImage = R.drawable.image_editor
+            }
+            "neptune" -> {
+                dummyImage = R.drawable.image_editor
+            }
+            "earth" -> {
+                dummyImage = R.drawable.image_editor
+            }
+            "moon" -> {
+                dummyImage = R.drawable.image_editor
+            }
+            "venus" -> {
+                dummyImage = R.drawable.image_editor
+            }
+            "jupiter" -> {
+                dummyImage = R.drawable.image_editor
+            }
+            "saturn" -> {
+                dummyImage = R.drawable.image_editor
+            }
+            "uranus" -> {
+                dummyImage = R.drawable.image_editor
+            }
+            "mercury" -> {
+                dummyImage = R.drawable.image_editor
+            }
+            "sun" -> {
+                dummyImage = R.drawable.image_editor
+            }
+        }
+        holder.robotimg.setImageResource(dummyImage!!)
     }
 
     override fun getItemCount(): Int {
-
+        return robot.size
     }
 }
--- app/src/main/java/com/example/roboticsapp/MainActivity.kt
+++ app/src/main/java/com/example/roboticsapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.roboticsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.roboticsapp.databinding.ActivityMainBinding
+import com.example.roboticsapp.databinding.ViewBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ViewBinding
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ViewBinding.inflate(layoutInflater)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        mainBinding.myRecycler.adapter = Adapter(binding, setData.SetPlanets())
+//        my_recycler.adapter=Adapter(setData.SetPlanets())
+        mainBinding.myRecycler.layoutManager = LinearLayoutManager(this)
     }
 }
