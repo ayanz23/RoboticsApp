ba567160a0a2da6f44debf802118cfdbcf9a4429
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2023-12-06 22:31:55 -0500

    make cards visibile

--- app/src/main/java/com/example/roboticsapp/MainActivity.kt
+++ app/src/main/java/com/example/roboticsapp/MainActivity.kt
@@ -5,20 +5,24 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roboticsapp.databinding.ActivityMainBinding
 import com.example.roboticsapp.databinding.ViewBinding
+import com.example.roboticsapp.Adapter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ViewBinding
     private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         binding = ViewBinding.inflate(layoutInflater)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
 
+        // Use the correct binding class
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
         mainBinding.myRecycler.adapter = Adapter(binding, setData.SetPlanets())
-//        my_recycler.adapter=Adapter(setData.SetPlanets())
         mainBinding.myRecycler.layoutManager = LinearLayoutManager(this)
     }
+
 }
