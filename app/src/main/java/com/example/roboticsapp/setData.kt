ba2a0db0f1ebf2fe6e7cac07a1f42008698e6ece
ravthegoat <asachdeva@ucvts.org>
2024-01-04 09:19:17 -0500

    almost done100

--- app/src/main/java/com/example/roboticsapp/setData.kt
+++ app/src/main/java/com/example/roboticsapp/setData.kt
@@ -12,7 +12,7 @@ object setData {
                 "Scotch Plains, New Jersey, USA",
                 "78",
                 "1257",
-                "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the \"Red Planet\".\nThe latter refers to the effect of the iron oxide prevalent on Mars's surface, which gives it a reddish appearance distinctive among the astronomical bodies visible to the naked eye.\nMars is a terrestrial planet with a thin atmosphere, with surface features reminiscent of the impact craters of the Moon and the valleys, deserts and polar ice caps of Earth."
+                "Team 1257 Parallel Universe is an unparalleled FIRST team that strives to inspire passion for STEM. We design and fabricate robots for the FIRST Robotics Competition (FRC) in order to develop talent in STEM and prepare a new generation of innovators, leaders, and entrepreneurs. Our aspiration is to create a community excited about STEM at the Union County Vocational-Technical Schools and beyond through FRC and our community outreach programs. We aim to create an environment where teamwork fuels creative problem solving."
             )
         )
         Robotlist.add(
@@ -22,7 +22,7 @@ object setData {
                 "Hightstown, New Jersey, USA",
                 "1",
                 "5895",
-                "The Moon is Earth's only natural satellite. At about one-quarter the diameter of Earth (comparable to the width of Australia), it is the largest natural satellite in the Solar System relative to the size of its planet, the fifth largest satellite in the Solar System overall, and is larger than any dwarf planet.\nOrbiting Earth at an average distance of 384,400 km (238,900 mi), or about 30 times Earth's diameter, its gravitational influence slightly lengthens Earth's day and is the main driver of Earth's tides. The Moon is classified as a planetary-mass object and a differentiated rocky body, and lacks any significant atmosphere, hydrosphere, or magnetic field.\nIts surface gravity is about one-sixth of Earth's (0.1654 g); Jupiter's moon Io is the only satellite in the Solar System known to have a higher surface gravity and density."
+                "FRC Team 5895, Peddie School Robotics, is an award-winning robotics team from Peddie School in Hightstown, New Jersey. We strive to create the next generation of leaders and innovators. The team is a part of The FIRST Robotics Competition (FRC), an international robotics competition consisting of more than 3700 teams across 28 countries. Peddie Robotics prepares students for higher education and the workforce by providing the opportunity to explore, develop, and pursue their passions. Peddie Robotics alumni are currently majoring in STEM fields at colleges and universities across the globe."
             )
         )
         Robotlist.add(
@@ -32,7 +32,7 @@ object setData {
                 "Clifton, New Jersey, USA",
                 "2",
                 "3314",
-                "Neptune is the eighth and farthest-known Solar planet from the Sun. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet.\n It is 17 times the mass of Earth, slightly more massive than its near-twin Uranus. Neptune is denser and physically smaller than Uranus because its greater mass causes more gravitational compression of its atmosphere. The planet orbits the Sun once every 164.8 years at an average distance of 30.1 AU (4.5 billion km; 2.8 billion mi).\nIt is named after the Roman god of the sea and has the astronomical symbol ♆, a stylised version of the god Neptune's trident."
+                "We are a FIRST Robotics Competition Team 3314, The Mechanical Mustangs, located in Clifton, New Jersey. Mechanical Mustangs  won the Autonomous Award."
             )
         )
         Robotlist.add(
@@ -42,7 +42,7 @@ object setData {
                 "Palmyra, Pennsylvania, USA",
                 "3",
                 "2539",
-                "Earth is the third planet from the Sun and the only astronomical object known to harbor and support life. About 29.2% of Earth's surface is land consisting of continents and islands. The remaining 70.8% is covered with water, mostly by oceans, seas, gulfs, and other salt-water bodies, but also by lakes, rivers, and other freshwater, which together constitute the hydrosphere.\n Much of Earth's polar regions are covered in ice. Earth's outer layer is divided into several rigid tectonic plates that migrate across the surface over many millions of years, while its interior remains active with a solid iron inner core, a liquid outer core that generates Earth's magnetic field, and a convective mantle that drives plate tectonics."
+                "Krypton Cougars is made up of sub-teams who all work together to produce a new, functional robot each season, all the while being innovative and facing unique game challenges."
             )
         )
         Robotlist.add(
@@ -52,7 +52,7 @@ object setData {
                 "Plainsboro, New Jersey, USA",
                 "4",
                 "1923",
-                "Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty. As the brightest natural object in Earth's night sky after the Moon, Venus can cast shadows and can be, on rare occasions, visible to the naked eye in broad daylight.\n Venus lies within Earth's orbit, and so never appears to venture far from the Sun, either setting in the west just after dusk or rising in the east a little while before dawn. Venus orbits the Sun every 224.7 Earth days.\n With a rotation period of 243 Earth days, it takes significantly longer to rotate about its axis than any other planet in the Solar System, and does so in the opposite direction to all but Uranus (meaning the Sun rises in the west and sets in the east). Venus does not have any moons, a distinction it shares only with Mercury among the planets in the Solar System."
+                "The MidKnight Inventors was established in the fall of 2005. The team was started by a small group of students at WW-P High School North, but expanded in 2009 to include students from High School South as well. Today, WW-P Robotics is one of the largest co-curricular activities in the district, with over 130 student team members representing both high schools in the West Windsor-Plainsboro Regional School District, as well as a large family of other FIRST programs for grades K-8."
             )
         )
         Robotlist.add(
@@ -62,7 +62,7 @@ object setData {
                 "Newton, New Jersey, USA",
                 "5",
                 "3142",
-                "Uranus is the seventh planet from the Sun. Its name is a reference to the Greek god of the sky, Uranus, who, according to Greek mythology, was the great-grandfather of Ares (Mars), grandfather of Zeus (Jupiter) and father of Cronus (Saturn).\nIt has the third-largest planetary radius and fourth-largest planetary mass in the Solar System. Uranus is similar in composition to Neptune, and both have bulk chemical compositions which differ from that of the larger gas giants Jupiter and Saturn. For this reason, scientists often classify Uranus and Neptune as \"ice giants\" to distinguish them from the other gas giants.\nUranus's atmosphere is similar to Jupiter's and Saturn's in its primary composition of hydrogen and helium, but it contains more \"ices\" such as water, ammonia, and methane, along with traces of other hydrocarbons. It has the coldest planetary atmosphere in the Solar System."
+                "Our high school team “opens” new and unique possibilities to students. Our robotics team provides students an exploration of twenty-first century careers and workforce, impressing upon them the importance of STEM, gracious professionalism, and service to the community. Our team members collaborate and conceptualize with engineers and mentors, who are employed in various companies, as well as members of our community that possess an expertise encompassing a multitude of disciplines. Upon the completion of the challenge allocated for the season, a dynamic and creative partnership is maintained well beyond the build season"
             )
         )
         Robotlist.add(
@@ -72,7 +72,7 @@ object setData {
                 "Skillman, New Jersey, USA",
                 "6",
                 "1403",
-                "The Sun is the star at the center of the Solar System. It is a nearly perfect sphere of hot plasma, heated to incandescence by nuclear fusion reactions in its core, radiating the energy mainly as visible light and infrared radiation. It is by far the most important source of energy for life on Earth. Its diameter is about 1.39 million kilometres (864,000 miles), or 109 times that of Earth. Its mass is about 330,000 times that of Earth; it accounts for about 99.86% of the total mass of the Solar System.\nRoughly three quarters of the Sun's mass consists of hydrogen (~73%); the rest is mostly helium (~25%), with much smaller quantities of heavier elements, including oxygen, carbon, neon and iron."
+                "Located in Skillman, NJ, FIRST Team 1403 Cougar Robotics has been demonstrating the importance of the STEM disciplines while also developing project management, leadership and teamwork skills since 2004."
             )
         )
         Robotlist.add(
@@ -82,7 +82,7 @@ object setData {
                 "West Chester, Pennsylvania, USA",
                 "7",
                 "1391",
-                "Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine and a half times that of Earth.\n It only has one-eighth the average density of Earth; however, with its larger volume, Saturn is over 95 times more massive. Saturn is named after the Roman god of wealth and agriculture; its astronomical symbol (♄) represents the god's sickle.\n The Romans named the seventh day of the week Saturday, Sāturni diēs (\"Saturn's Day\") no later than the 2nd century for the planet Saturn."
+                "We are a team of thinkers. Of collaborators. Of innovators. The Metal Moose is the robotics team of Westtown School, a Pre-K to 12th Grade Quaker school in West Chester, Pennsylvania. The Metal Moose is a member of FIRST (For Inspiration and Recognition of Science and Technology), an international organization that promotes involvement in science and technology for youth of all ages."
             )
         )
         Robotlist.add(
@@ -92,7 +92,7 @@ object setData {
                 "Carneys Point, New Jersey, USA",
                 "8",
                 "316",
-                "Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass (more than) two and a half times that of all the other planets in the Solar System combined, but (a little) less than one-thousandth the mass of the Sun.\nJupiter is the third-brightest natural object in the Earth's night sky after the Moon and Venus. It has been observed since pre-historic times and is named after the Roman god Jupiter, the king of the gods, because of its massive size"
+                "The LuNaTeCs (Learning Under Nurturing Adults Teaching Engineering Concepts and Skills), FIRST® Team 316, became a team in 1999 at Salem  Community College with seven students and four mentors through a partnership with local businesses and educators. Our students come from high schools throughout Salem, Cumberland, and Gloucester Counties which are located in a rural, agricultural area located in the southwest corner of New Jersey. The LuNaTeCs remain one of the smaller teams in FIRST with 10-20 students. The small size of our team allows each team member to be an integral part of the robot design, build and strategy, getting “hands on” experience.  Every team member counts. In 2008 the team incorporated to a not-for-profit organization – South Jersey Robotics, Inc."
             )
         )
         Robotlist.add(
@@ -102,7 +102,7 @@ object setData {
                 "Montvale, New Jersey, USA",
                 "9",
                 "1676",
-                "Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. It is named after the Roman god Mercurius (Mercury), god of commerce, messenger of the gods, and mediator between gods and mortals, corresponding to the Greek god Hermes (Ἑρμῆς).\nLike Venus, Mercury orbits the Sun within Earth's orbit as an inferior planet, and its apparent distance from the Sun as viewed from Earth never exceeds 28°.\nThis proximity to the Sun means the planet can only be seen near the western horizon after sunset or the eastern horizon before sunrise, usually in twilight. At this time, it may appear as a bright star-like object but is often far more difficult to observe than Venus.\nFrom Earth, the planet telescopically displays the complete range of phases, similar to Venus and the Moon, which recurs over its synodic period of approximately 116 days."
+                "The Pascack Pi-oneers, located in Bergen County, New Jersey, are part of the Pascack Valley Regional High School District, which consists of four towns: Hillsdale, River Vale, Montvale and Woodcliff Lake. Students from Hillsdale and River Vale attend Pascack Valley High School, students from Montvale and Woodcliff Lake attend Pascack Hills High School. Team meetings are held at Pascack Hills and through the generous support of the PVRHSD Board of Education, members from Pascack Valley are transported to Pascack Hills by bus. "
             )
         )
 
