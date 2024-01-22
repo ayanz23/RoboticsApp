f6d28b91e44e0005043f92b415497aac74517bb4
ayanz23 <ayanzia10@gmail.com>
2024-01-21 22:13:13 -0500

    LETS GO

--- app/src/main/java/com/example/roboticsapp/AddTeam.kt
+++ app/src/main/java/com/example/roboticsapp/AddTeam.kt
@@ -18,14 +18,17 @@ class AddTeam : AppCompatActivity() {
         submitTeamButton.setOnClickListener {
             val teamName = findViewById<EditText>(R.id.editTeamName).text.toString()
             val teamNumber = findViewById<EditText>(R.id.editTeamNumber).text.toString()
+            val teamRank = findViewById<EditText>(R.id.editTeamRank).text.toString()
+            val teamLocation = findViewById<EditText>(R.id.editTeamLocation).text.toString()
+            val teamDescription = findViewById<EditText>(R.id.editTeamDescription).text.toString()
 
             val newRobot = RobotData(
                 setData.getLastRobotID(),
                 teamName,
-                "Scotch Plains, New Jersey, USA",
-                "78",
+                teamLocation,
+                teamRank,
                 teamNumber,
-                "Team 1257 Parallel Universe is an unparalleled FIRST team that strives to inspire passion for STEM. We design and fabricate robots for the FIRST Robotics Competition (FRC) in order to develop talent in STEM and prepare a new generation of innovators, leaders, and entrepreneurs. Our aspiration is to create a community excited about STEM at the Union County Vocational-Technical Schools and beyond through FRC and our community outreach programs. We aim to create an environment where teamwork fuels creative problem solving."
+                teamDescription
             )
 
             System.out.println("New robot: " + newRobot.toString())
